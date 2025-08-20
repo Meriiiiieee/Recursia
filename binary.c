729a91264eb7bee7a25b69346cd7be5e1e22bc7e
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-08-20 21:15:29 +0400

    binary representation of number

---
+++ binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void binary(int n) {
+    if (n == 0 || n == 1 ){
+        printf("%d", n);
+        return ;
+    }
+    
+    if (n>1){
+    binary(n/2);
+    }
+    printf("%d", n%2);
+} 
+
+
+int main() {
+    int n = 0;
+    
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    
+     binary(n);
+    return 0;
+}
